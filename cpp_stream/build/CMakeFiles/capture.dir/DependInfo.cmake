13bf4501ca369666c2cc09591a03afb257bfcdde
Ricky Zhang <ricky.yuxiang.zhang@gmail.com>
2019-02-07 14:50:19 -0800

    modified cpp_stream with align

--- cpp_stream/build/CMakeFiles/capture.dir/DependInfo.cmake
+++ cpp_stream/build/CMakeFiles/capture.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/csnesummer/Documents/realsense/capture/cpp_stream/capture.cpp" "/home/csnesummer/Documents/realsense/capture/cpp_stream/build/CMakeFiles/capture.dir/capture.cpp.o"
+  "/home/csnesummer/Documents/realsense/RealsenseCapture/cpp_stream/capture.cpp" "/home/csnesummer/Documents/realsense/RealsenseCapture/cpp_stream/build/CMakeFiles/capture.dir/capture.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cpp_stream/capture.cpp
+++ cpp_stream/capture.cpp
@@ -5,36 +5,47 @@
 
 using namespace cv;
 
+rs2_stream find_stream_to_align(const std::vector<rs2::stream_profile>& streams);
+bool profile_changed(const std::vector<rs2::stream_profile>& current, const std::vector<rs2::stream_profile>& prev);
+
 int main(){
 
     // Create a Pipeline - this serves as a top-level API for streaming and processing frames
-    rs2::pipeline pipeline;
+    rs2::pipeline pipe;
     rs2::config config;
- // 	config.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 15);
-	// config.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 15);
     config.enable_stream(RS2_STREAM_DEPTH, 1280, 720, RS2_FORMAT_Z16, 15);
-	config.enable_stream(RS2_STREAM_COLOR, 1280, 720, RS2_FORMAT_BGR8, 15);
+    config.enable_stream(RS2_STREAM_COLOR, 1280, 720, RS2_FORMAT_BGR8, 15);
 
 
     // Configure and start the pipeline
-    // pipeline.start();
-    rs2::pipeline_profile profile = pipeline.start(config);
+    rs2::pipeline_profile profile = pipe.start(config);
 
-    std::cout << "Streaming..." << std::endl;
+    rs2_stream align_to = find_stream_to_align(profile.get_streams());
+	
+    rs2::align align(align_to);
+    
+    std::cout << "Now Streaming.." << std::endl;
 
     int frame_count = 0;
 
-    while (frame_count < 100000)
+    while (frame_count < 1000000)
     {
         // Block program until frames arrive
-        rs2::frameset frames = pipeline.wait_for_frames();
+        rs2::frameset frameset = pipe.wait_for_frames();
 
+		if (profile_changed(pipe.get_active_profile().get_streams(), profile.get_streams()))
+		{
+			profile = pipe.get_active_profile();
+			align_to = find_stream_to_align(profile.get_streams());
+			align = rs2::align(align_to);
+		}
+
+		auto processed = align.process(frameset);
+	
         // Try to get a frame of a depth image
-        rs2::depth_frame depth_frame = frames.get_depth_frame();
-        rs2::video_frame color_frame = frames.get_color_frame();
+        rs2::depth_frame depth_frame = processed.get_depth_frame();
+        rs2::video_frame color_frame = processed.get_color_frame();
 
-  		// const int w = depth_frame.as<rs2::video_frame>().get_width();
-		// const int h = depth_frame.as<rs2::video_frame>().get_height();
 
         // Get the depth frame's dimensions
         int w = depth_frame.get_width();
@@ -46,10 +57,10 @@ int main(){
 
 		frame_count++;
 
-		if (frame_count > 50 && frame_count%1 == 0){
+		if (frame_count > 50){
 
 			std::ostringstream filename;
-			filename << std::setw(7) << std::setfill('0') << frame_count;
+			filename << std::setw(5) << std::setfill('0') << frame_count;
 
 			// cv::imwrite("./rgb/"+std::to_string(filename)+".png", color_image);
 			// cv::imwrite("./depth/"+std::to_string(filename)+".png", depth_image);
@@ -58,20 +69,69 @@ int main(){
 		}
 
 
-        // Print the frame count
-        if (frame_count % 200 == 0){
-        std::cout << "frame count: "+ std::to_string(frame_count) << std::endl;
-    	}
-
-
-        // std::cout << "timestamp: " + std::to_string(depth_frame.get_timestamp()) << std::endl;
+        // Print the frame count at checkpoints
+	if (frame_count % 500 == 0)
+	{
+        	std::cout << "frame count: "+ std::to_string(frame_count) << std::endl;
+	}
     }
 
 	auto sensor = profile.get_device().first<rs2::depth_sensor>();
 	auto scale =  sensor.get_depth_scale();
 	std::cout << "depth scale: "+ std::to_string(scale) << std::endl;
 
-    pipeline.stop();
+    pipe.stop();
 
     return 0;
+}
+
+rs2_stream find_stream_to_align(const std::vector<rs2::stream_profile>& streams)
+{
+    //Given a vector of streams, we try to find a depth stream and another stream to align depth with.
+    //We prioritize color streams to make the view look better.
+    //If color is not available, we take another stream that (other than depth)
+    rs2_stream align_to = RS2_STREAM_ANY;
+    bool depth_stream_found = false;
+    bool color_stream_found = false;
+    for (rs2::stream_profile sp : streams)
+    {
+        rs2_stream profile_stream = sp.stream_type();
+        if (profile_stream != RS2_STREAM_DEPTH)
+        {
+            if (!color_stream_found)         //Prefer color
+                align_to = profile_stream;
+
+            if (profile_stream == RS2_STREAM_COLOR)
+            {
+                color_stream_found = true;
+            }
+        }
+        else
+        {
+            depth_stream_found = true;
+        }
+    }
+
+    if(!depth_stream_found)
+        throw std::runtime_error("No Depth stream available");
+
+    if (align_to == RS2_STREAM_ANY)
+        throw std::runtime_error("No stream found to align with Depth");
+
+    return align_to;
+}
+
+
+bool profile_changed(const std::vector<rs2::stream_profile>& current, const std::vector<rs2::stream_profile>& prev)
+{
+    for (auto&& sp : prev)
+    {
+        //If previous profile is in current (maybe just added another)
+        auto itr = std::find_if(std::begin(current), std::end(current), [&sp](const rs2::stream_profile& current_sp) { return sp.unique_id() == current_sp.unique_id(); });
+        if (itr == std::end(current)) //If it previous stream wasn't found in current
+        {
+            return true;
+        }
+    }
+    return false;
 }
